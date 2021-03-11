8a67f81bee15ef411abf1b99b24e342672f83b1a
qetmes <55390882+qetmes@users.noreply.github.com>
2021-03-11 15:44:59 +0800

    Add files via upload

---
+++ 002-springboot-springmvc/readme.txt
@@ -0,0 +1,2 @@
+使用springboot框架集成springmvc：
+    只要创建好springboot项目,则springmvc也就集成完毕了。
---
+++ 002-springboot-springmvc/src/main/java/com/guo/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.guo.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/springboot/say")
+    @ResponseBody
+    public String say(){
+        return "Hello,SpringBoot";
+    }
+}
